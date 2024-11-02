d64b37217ce03a4cfc6ecc682e1d7c97ccd8b1f3
Yigit Gulay <yigitgulay11@outlook.com>
2024-11-02 20:20:36 +0300

    Add the forgoten .gitignore

--- src/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.all("/", (req, res) => res.send("Compute Controls under development."))
-
-app.listen(3100, () => console.log("App is being listened at port 3100"))
