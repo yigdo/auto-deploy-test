0d75a03e66b67ec99da1de37756cd73d1ac327a5
Yigit Gulay <80751083+yigdo@users.noreply.github.com>
2024-11-03 13:28:49 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const app = require('express')();
 
-app.all('*', (req, res) => res.status(200).send('Welcome to Auto-Deploy-Test!'));
+app.all('*', (req, res) => res.status(200).send('Welcome to AutoDeployTest!'));
 
 app.listen(process.env.PORT ?? 3030, () => console.log('Listening on port ' + (process.env.PORT ?? 3030)));
